76ecbb4d3b4fee6311b471b4cdcbdc2735527e29
Dillen Roggensinger <dillen.roggensinger@gmail.com>
2012-05-02 22:31:20 -0400

    Fixed compound query from AND to and. Stupid case sensitivity.

--- QueryResultFormatter.pm
+++ QueryResultFormatter.pm
@@ -162,6 +162,7 @@ sub get_possible_albums {
 	}
 	do {	#Loop through all the offsets until no more results are found
 		my $url = MusicBrainzQuerier::formulate_search_query($offset, 'release-group','release-group',$name,'type','album|compilation|soundtrack|live');
+		print $url;
 		$ref = MusicBrainzQuerier::search($url);
 		foreach my $id (keys %$ref){
 			$recordings_and_ids->{$id} = {
