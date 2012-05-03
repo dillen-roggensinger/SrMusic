10b9f9e632842dd65646f69691adb2f2d12ea625
Eli Katz <friendlyneighborhoodkatz@gmail.com>
2012-05-03 02:30:25 -0400

    Songs query working

--- web/returnsearch.pl
+++ web/returnsearch.pl
@@ -12,14 +12,14 @@ use QueryResultFormatter;
 
 sub artist {
 	my $ref;
-	(my $search_for, my $search_by) = @_;
+	( my $search_for, my $search_by ) = @_;
 	$ref = QueryResultFormatter::get_possible_artists($search_for);
-	
+
 	my $template = HTML::Template->new( filename => 'templates/artist.html' );
-	
-	$template->param(SEARCH_FOR => $search_for);
-	$template->param(SEARCH_BY => $search_by);
-	
+
+	$template->param( SEARCH_FOR => $search_for );
+	$template->param( SEARCH_BY  => $search_by );
+
 	my @artists;
 
 	foreach my $key ( keys %{$ref} ) {
@@ -32,70 +32,112 @@ sub artist {
 			}
 		);
 	}
-	
-	$template->param(SONG_INFO => [@artists]);
+
+	$template->param( SONG_INFO => [@artists] );
 
 	print "Content-Type: text/html\n\n", $template->output;
 }
 
 sub song {
 	my $ref;
-	(my $search_for, my $search_by) = @_;
+	( my $search_for, my $search_by ) = @_;
 	QueryResultFormatter::get_possible_recordings($search_for);
-	my $template = HTML::Template->new( filename => 'templates/returnsearch.html' );
-	$template->param(SEARCH_FOR => $search_for);
-	$template->param(SEARCH_BY => $search_by);
-	$template->param(
-		SONG_INFO => [
-			{ ARTIST => 'dillen', ALBUM => 'sucks', SONG => 'FUCK MY NUTS'},
-			{ ARTIST => 'Terrance', ALBUM => 'greatest hits', SONG => 'Fuck you'},
-		]
-	);
+	my $template =
+	  HTML::Template->new( filename => 'templates/songs.html' );
+	$template->param( SEARCH_FOR => $search_for );
+	$template->param( SEARCH_BY  => $search_by );
+
+	$ref = QueryResultFormatter::get_possible_recordings('Party Rock');
+	my @songs;
+	foreach my $id ( keys %{$ref} ) {
+		my $SONGID = $id;
+		my $ALBUMID;
+		my $ALBUMTITLE;
+		my $ARTISTNAME;
+		my $ARTISTID;
+		if ( defined( $ref->{$id}{'album'}{'id'} ) ) {
+			$ALBUMID = $ref->{$id}{'album'}{'id'};
+		}
+		else {
+			next;
+		}
+		if ( defined( $ref->{$id}{'album'}{'title'} ) ) {
+			$ALBUMTITLE = $ref->{$id}{'album'}{'title'};
+		}
+		else {
+			next;
+		}
+		if ( ref( $ref->{$id}{'artists'}[0] ) eq 'HASH' ) {
+			$ARTISTNAME = $ref->{$id}{'artists'}[0]{'artist'}{'name'};
+			$ARTISTID   = $ref->{$id}{'artists'}[0]{'artist'}{'id'};
+		}
+		else {
+			next;
+		}
+		my $SONGTITLE = $ref->{$id}{'title'};
+		my $SONGSCORE = $ref->{$id}{'ext:score'};
+		push(
+			@songs,
+			{
+				SONGID     => $SONGID,
+				ALBUMID    => $ALBUMID,
+				ALBUMTITLE => $ALBUMTITLE,
+				ARTISTNAME => $ARTISTNAME,
+				ARTISTID   => $ARTISTID,
+				SONGTITLE  => $SONGTITLE,
+				SONGSCORE  => $SONGSCORE,
+			}
+		);
+	}
+	$template->param( SONG_INFO => [@songs] );
 
 	print "Content-Type: text/html\n\n", $template->output;
 }
 
 sub album {
 	my $ref;
-	(my $search_for, my $search_by) = @_;
+	( my $search_for, my $search_by ) = @_;
 	QueryResultFormatter::get_possible_albums($search_for);
-	my $template = HTML::Template->new( filename => 'templates/returnsearch.html' );
-	$template->param(SEARCH_FOR => $search_for);
-	$template->param(SEARCH_BY => $search_by);
+	my $template =
+	  HTML::Template->new( filename => 'templates/returnsearch.html' );
+	$template->param( SEARCH_FOR => $search_for );
+	$template->param( SEARCH_BY  => $search_by );
 	$template->param(
 		SONG_INFO => [
-			{ ARTIST => 'dillen', ALBUM => 'sucks', SONG => 'FUCK MY NUTS'},
-			{ ARTIST => 'Terrance', ALBUM => 'greatest hits', SONG => 'Fuck you'},
+			{ ARTIST => 'dillen', ALBUM => 'sucks', SONG => 'FUCK MY NUTS' },
+			{
+				ARTIST => 'Terrance',
+				ALBUM  => 'greatest hits',
+				SONG   => 'Fuck you'
+			},
 		]
 	);
 
 	print "Content-Type: text/html\n\n", $template->output;
 }
 
-
 sub returnSearch {
-	(my $search_for, my $search_by) = @_;
+	( my $search_for, my $search_by ) = @_;
 	my $ref;
-	if ($search_by eq "artist") {
-		artist($search_for, $search_by);
+	if ( $search_by eq "artist" ) {
+		artist( $search_for, $search_by );
 	}
-	elsif ($search_by eq "song") {
-		song($search_for, $search_by);
+	elsif ( $search_by eq "song" ) {
+		song( $search_for, $search_by );
 	}
-	else { #Default to Album
-		album($search_for, $search_by);
+	else {    #Default to Album
+		album( $search_for, $search_by );
 	}
-	
-	
+
 }
 
 my $query      = new CGI;
 my $search_for = $query->param("main-search-text");
 my $search_by  = $query->param("main-search-by");
 
-if ($search_for eq "" || $search_by eq "") {
+if ( $search_for eq "" || $search_by eq "" ) {
 	Helper::error("No Parameters Set in CGI");
-} 
+}
 else {
-	returnSearch($search_for, $search_by);
+	returnSearch( $search_for, $search_by );
 }
