cf472044f707382142e5528ea122ae28070b9530
Dillen Roggensinger <dillen.roggensinger@gmail.com>
2012-05-04 02:11:55 -0400

    My testing scripts

--- testing_scripts/test_formatter.pl
+++ testing_scripts/test_formatter.pl
@@ -7,23 +7,26 @@ use QueryResultFormatter;
 use Data::Dumper;
 use URI::Escape;
 
-#my $hash = QueryResultFormatter::get_possible_artists('bruce');
+#my $hash = QueryResultFormatter::get_possible_artists('LMFAO');
 #print Dumper($hash);
 
-#my $hash = QueryResultFormatter::get_possible_recordings('layla');
+#my $hash = QueryResultFormatter::get_possible_recordings('under pressure');
 #print Dumper($hash);
 
 #my $hash = QueryResultFormatter::get_possible_albums('take off your pants and jacket');
 #print Dumper($hash);
 
-#my $id = QueryResultFormatter::get_artist('recording', 'd0bd2a62-5dd8-49bf-ae51-8872a49184c0');
+#my $id = QueryResultFormatter::get_artist_id('recording', 'd0bd2a62-5dd8-49bf-ae51-8872a49184c0');
 #print Dumper($id);
 
-#my $id = QueryResultFormatter::get_release('02c2b0c7-065d-38b4-8ac0-3391839f2418');
+#my $id = QueryResultFormatter::get_release_id('02c2b0c7-065d-38b4-8ac0-3391839f2418');
 #print Dumper($id);
 
-#my $hash = QueryResultFormatter::get_albums('618b6900-0618-4f1e-b835-bccb17f84294');
+my $hash = QueryResultFormatter::get_albums('f46bd570-5768-462e-b84c-c7c993bbf47e');
+print Dumper($hash);
+
+#my $hash = QueryResultFormatter::get_songs('d33aadd5-bcc2-3531-b96d-378da2e04a4e'); #f5201f3a-aea3-3f19-93d9-d89d3d976f67 02c2b0c7-065d-38b4-8ac0-3391839f2418
 #print Dumper($hash);
 
-my $hash = QueryResultFormatter::get_songs('02c2b0c7-065d-38b4-8ac0-3391839f2418');
-print Dumper($hash);
+#my $hash = QueryResultFormatter::get_artists('d0bd2a62-5dd8-49bf-ae51-8872a49184c0');
+#print Dumper($hash);
