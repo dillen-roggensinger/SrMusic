5b2395e6cd67c4c3db6ab83c412e921b23d802e9
Eli Katz <friendlyneighborhoodkatz@gmail.com>
2012-05-03 20:06:46 -0400

    blah

--- testing_scripts/test_formatter.pl
+++ testing_scripts/test_formatter.pl
@@ -6,11 +6,11 @@ use strict;
 use QueryResultFormatter;
 use Data::Dumper;
 
-my $hash = QueryResultFormatter::get_possible_artists('bruce');
-print Dumper($hash);
-
-#my $hash = QueryResultFormatter::get_possible_recordings('layla');
+#my $hash = QueryResultFormatter::get_possible_artists('bruce');
 #print Dumper($hash);
+
+my $hash = QueryResultFormatter::get_possible_recordings('layla');
+print Dumper($hash);
 
 #my $hash = QueryResultFormatter::get_possible_albums('take off your pants and jacket');
 #print Dumper($hash);
